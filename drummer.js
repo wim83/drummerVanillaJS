8046c917b327bc55b15aa19745f85a13aa93c202
Wim83 <wimva@mail.com>
2017-10-16 21:40:53 +0200

    connectivity test

---
+++ drummer.js
@@ -0,0 +1,11 @@
+// window.onload = init;
+
+// function init(){
+// window.on('keypress',function(event){
+//     console.log(event);
+// })
+// }
+
+document.addEventListener('keydown', function (e) {
+    console.log(e);
+})
