7127ac09e5e4da4d525cf118bdf82a7ed93e7d7c
Wim83 <wimva@mail.com>
2017-10-16 21:59:47 +0200

    playing

--- drummer.js
+++ drummer.js
@@ -8,4 +8,12 @@
 
 document.addEventListener('keydown', function (e) {
     console.log(e);
+    var number = e.keyCode;
+    console.log(number);
+    var audio = document.querySelector('audio[data-key="' + number + '"]');
+    var div = document.querySelector('div[data-key="' + number + '"]');
+    console.log(audio);
+    audio.play();
+    console.log(div);
+
 })
